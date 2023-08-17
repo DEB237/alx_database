2af57ba32a05641065569794c28f3ac99dfb43b7
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-08-17 21:28:37 +0100

    SQL introduction

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,14 +1,15 @@
--- Script to list all tables of a database in MySQL server
-   
--- Check if the database name is provided as an argument
+#!/bin/bash
+
+# Script to list all tables of a database in MySQL server
+
+# Check if the database name is provided as an argument
 if [ $# -eq 0 ]; then
-     echo "Please provide the database name as an argument."
-     exit 1
+    echo "Please provide the database name as an argument."
+    exit 1
 fi
-   
--- Assign the first argument to a variable representing the database name
+
+# Assign the first argument to a variable representing the database name
 dbname=$1
-   
-   
--- Execute the query to list all tables of the specified database
+
+# Execute the MySQL command to list all tables of the specified database
 mysql -u <username> -p -e "USE $dbname; SHOW TABLES;"
