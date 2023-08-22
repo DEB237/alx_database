ac099199196694de2357323b5492599563d51745
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-08-22 23:11:36 +0100

    SQL introduction

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,11 +1,8 @@
 -- Script lists all the tables of a database in a MySQL server.
 -- The database name is passed as argument of mysql command (in the following example: mysql is the name of the database)
 
--- Connect to the MySQL server.
-CONNECT root@localhost;
-
 -- Get the database name from the command line arguments.
-SET @database_name = 'hbtn_test_db';
+SET @database_name = 'mysql';
 
 -- List the tables in the database.
 SELECT
