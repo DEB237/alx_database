42d4ca950c36d70314890898753bdfca8f54c8b5
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-08-30 16:13:36 +0100

    SQL Introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,6 @@
 -- Prints the full description of the table first_table
 
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = database AND TABLE_NAME = table
+SELECT column_name, data_type, data_length, nullable
+FROM all_tab_columns
+WHERE table_name = first_table;
    
