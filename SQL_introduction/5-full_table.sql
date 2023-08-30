436b9da42baef789a79e5fee96b04772b3616367
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-08-30 16:21:24 +0100

    SQL Introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
 -- Prints the full description of the table first_table
 
-SELECT dbms_metadata.get_ddl('TABLE', 'first_table', 'hbtn_0c_0') FROM dual;
+SELECT dbms_metadata.get_ddl('TABLE', first_table, hbtn_0c_0) FROM dual;
 
    
