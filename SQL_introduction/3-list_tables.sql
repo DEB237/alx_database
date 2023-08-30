50457320d06bdc8297d40c8183e9db9f061f18c0
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-08-30 11:26:11 +0100

    SQL Introduction

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,15 +1,18 @@
--- Script lists all the tables of a database in a MySQL server.
--- The database name is passed as argument of mysql command (in the following example: mysql is the name of the database)
+#!/bin/bash
 
--- Get the database name from the command line arguments.
-SET @database_name = 'mysql';
+# Script to list table names in a database without using SELECT or SHOW
 
--- List the tables in the database.
-SHOW TABLE
-    table_name
-FROM
-    information_schema.tables
-WHERE
-    database_name = @database_name
-ORDER BY
-    table_name;
+# Check if the database name is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Please provide the database name as an argument."
+    exit 1
+fi
+
+# Assign the first argument to a variable representing the database name
+dbname=$1
+
+# Execute the query to retrieve table names without using SELECT or SHOW
+# The query uses the information_schema.tables view to get table names
+# The result is extracted from the output using awk
+mysql -u <username> -p --skip-column-names --batch -e "USE $dbname; DESCRIBE \`dummy\`;" \
+    | awk '{print $1}'
--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,17 +1,14 @@
-#!/bin/bash
-
-# Script to delete a database in MySQL server
-
-# Check if the database name is provided as an argument
+-- Script to create a table called first_table in the current database in MySQL server
+   
+   
+-- Check if the database name is provided as an argument
 if [ $# -eq 0 ]; then
-    echo "Please provide the database name as an argument."
-    exit 1
+     echo "Please provide the database name as an argument."
+     exit 1
 fi
-
-# Assign the first argument to a variable representing the database name
+   
+-- Assign the first argument to a variable representing the database name
 dbname=$1
-
-# Execute the query to delete the specified database
-# The -e flag is used to execute the provided SQL query
-# DROP DATABASE IF EXISTS is used to delete the database if it exists
-mysql -u <username> -p -e "DROP DATABASE IF EXISTS $dbname;"
+   
+-- Execute the query to create the table if it doesn't exist
+mysql -u <username> -p -e "USE $dbname;" -e "CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));"
