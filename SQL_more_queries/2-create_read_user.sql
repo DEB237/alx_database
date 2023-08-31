e36e4ee4a8018528eadc5534697d84ea18de9955
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-08-31 13:05:09 +0100

    SQL more queries

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -5,5 +5,5 @@ CREATE SCHEMA IF NOT EXISTS hbtn_0d_2;
 
 -- creates the user_0d_2 with password user_0d_2_pwd
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON `htbn_0d_2`.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
